70bc31514466581ed6c0190e4ac46cb8ef528bcd
Elvan Hidayat <elvanhd@gmail.com>
2023-07-03 19:29:17 +0700

    create products and shopping-cart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index'])->name('home');
+
+Route::get('/shop', [ProductController::class, 'index'])->name('shop');
+Route::get('/shop/{id}', [ProductController::class, 'show'])->name('product');
+
+Route::get('/cart', [CartController::class, 'cart'])->name('cart');
+Route::get('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/delete-from-cart/{id}', [CartController::class, 'delete'])->name('delete.from.cart');
